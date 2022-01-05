b156ae1216846b8e77ba1af71d680de75ef82eb6
Zoe Ji <student@bchd.a79f71ce-7c9d-4932-8de2-01adbb3573c8>
2022-01-05 21:32:34 +0000

    writing custom code

--- cust_if_script.py
+++ cust_if_script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Alta3 Research | RZFeeser
-   if, elif, else - A simple program using conditionals to make a decision."""
+   if, elif, else - A program that prompts a user for a numeric score, then returns the letter grade associated with that score."""
 
 
 message = 'The letter grade associated with the score you entered is: '
@@ -9,15 +9,15 @@ message = 'The letter grade associated with the score you entered is: '
 score = float(input("What is your numeric score?"))
 
 
-if score >= 90 and score <=100:
+if score >= 90 and score <= 100:
     message = message + 'A.'
 elif score >= 80 and score <= 89:
     message = message + 'B.'
-elif score >= 70 and score <=79:
+elif score >= 70 and score <= 79:
     message = message + 'C.'
-elif score >=60 and score <=69:
+elif score >= 60 and score <= 69:
     message = message + 'D.'
-elif score <=59:
+elif score <= 59:
     message = message + 'F.'
 else:
     message = 'The score you entered is invalid. Please enter number between 0 and 100.'
