5b3a220f344a1f7878e3d7f98bfec902a5d2b057
Zoe Ji <student@bchd.a79f71ce-7c9d-4932-8de2-01adbb3573c8>
2022-01-06 07:00:17 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+my_list = ["192.168.0.5", 5060, "UP"]
+print("The first item is: " + my_list[0])
+print("The second item is: " +str(my_list[1]))
+print("The last item is: " + my_list[2])
+
+iplist=[5060, "80", 55, "10.0.0.1", "10.20.30.1","ssh"]
+print("The IP addresses in the list is: "+iplist[3]+" and "+iplist[4])
