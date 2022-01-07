67f6529c2857631596f9e7e898177fa788efd6f7
Zoe Ji <student@bchd.a79f71ce-7c9d-4932-8de2-01adbb3573c8>
2022-01-07 17:37:06 +0000

    challenges on 1/7/2022

---
+++ challenges.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# 1.Write a Python function to find the Max of three numbers.
+def maxOfThreeNum(num1:float, num2:float, num3:float):
+    a = (num1,num2,num3)
+    x = sorted(a,reverse=True)
+    print(x[0])
+maxOfThreeNum(2.1,5.7,9.6)
+
+def maxOfThreeNum2(num1:float, num2:float, num3:float):
+    a = (num1, num2, num3)
+    x = max(a)
+    print(x)
+maxOfThreeNum2(2.1,5.7,9.6)
+
+# 2.Write a Python function to sum all the numbers in a list.
+def sumOfAll(num:list):
+    sumNum = sum(num)
+    print(sumNum)
+sumOfAll([8,2,3,0,7])
+
+# 3.Write a Python function to multiply all the numbers in a list.
+def mult(nums:list):
+    result = 1
+    for num in nums:
+        result = result * num
+    print (result)
+mult([8,2,3,-1,7])
+
+# 5.Write a Python function to check whether a number is in a given range.
+def numInRange(num:int,rangeMin:int=0, rangeMax:int=10):
+    if (num >= rangeMin and num <= rangeMax):
+        print(f"{num} is in the range {rangeMin} to {rangeMax}")
+    else:
+        print(f"{num} is not in the range {rangeMin} to {rangeMax}")
+
+numInRange(12)
+numInRange(5)
+
+
+# 6.Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
+
